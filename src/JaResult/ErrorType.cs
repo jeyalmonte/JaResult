7646c2599a3bd0832e4659305207e3972a9d9fe7
Jeyson Almonte <jjalmonte@humano.com>
2025-05-09 00:41:07 -0400

    docs(ErrorType): add XML documentation

--- src/JaResult/ErrorType.cs
+++ src/JaResult/ErrorType.cs
@@ -1,16 +1,43 @@
 ﻿namespace JaResult;
 
-/// <summary>
-/// Represents the type of an error returned in a result.
-/// </summary>
+/// <summary>  
+/// Represents the type of an error returned in a result.  
+/// </summary>  
 public enum ErrorType
 {
+	/// <summary>  
+	/// Represents a general failure.  
+	/// </summary>  
 	Failure,
+
+	/// <summary>  
+	/// Represents a validation error.  
+	/// </summary>  
 	Validation,
+
+	/// <summary>  
+	/// Represents an unexpected error.  
+	/// </summary>  
 	Unexpected,
+
+	/// <summary>  
+	/// Represents a conflict error.  
+	/// </summary>  
 	Conflict,
+
+	/// <summary>  
+	/// Represents a not found error.  
+	/// </summary>  
 	NotFound,
+
+	/// <summary>  
+	/// Represents an unauthorized error.  
+	/// </summary>  
 	Unauthorized,
+
+	/// <summary>  
+	/// Represents a forbidden error.  
+	/// </summary>  
 	Forbidden,
 }
 
